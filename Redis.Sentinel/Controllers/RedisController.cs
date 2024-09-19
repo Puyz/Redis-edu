4b1665a75b44a7bf41922096b3b0427424b0e178
Ömer temel <42616536+Puyz@users.noreply.github.com>
2024-09-19 13:23:37 +0300

    redis sentinel

---
+++ Redis.Sentinel/Controllers/RedisController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Redis.Sentinel.Services;
+using StackExchange.Redis;
+
+namespace Redis.Sentinel.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RedisController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetValue(string key)
+        {
+            IDatabase redisDB = await RedisService.GetRedisMasterDatabase();
+            RedisValue value = await redisDB.StringGetAsync(key);
+            return Ok(value.ToString());
+        }
+
+        [HttpPut("[action]")]
+        public async Task<IActionResult> SetValue(string key, string value)
+        {
+            IDatabase redisDB = await RedisService.GetRedisMasterDatabase();
+            await redisDB.StringSetAsync(key, value);
+            return Ok();
+        }
+    }
+}
---
+++ Redis.Sentinel/Services/RedisService.cs
@@ -0,0 +1,58 @@
+﻿using StackExchange.Redis;
+
+namespace Redis.Sentinel.Services
+{
+    public class RedisService
+    {
+        static ConfigurationOptions sentinelOptions => new()
+        {
+            // Sentinel sunucularımız
+            EndPoints =
+            {
+                {"localhost", 6383 },
+                {"localhost", 6384 },
+                {"localhost", 6385 },
+            },
+
+            CommandMap = CommandMap.Sentinel,
+            AbortOnConnectFail = false,
+        };
+
+        static ConfigurationOptions masterOptions => new()
+        {
+            AbortOnConnectFail = false,
+        };
+
+        public static async Task<IDatabase> GetRedisMasterDatabase()
+        {
+            ConnectionMultiplexer sentinelConnection = await ConnectionMultiplexer.SentinelConnectAsync(sentinelOptions);
+
+            System.Net.EndPoint masterEndPoint = null;
+
+            foreach (System.Net.EndPoint endPoint in sentinelConnection.GetEndPoints())
+            {
+                IServer server = sentinelConnection.GetServer(endPoint);
+                if (!server.IsConnected)
+                    continue;
+
+                // mymaster -> sentinel.conf dosyası içerisinde belirtilen master ismi
+                masterEndPoint = await server.SentinelGetMasterAddressByNameAsync("mymaster");
+                break;
+            }
+
+            // Bize docker'daki internal IP'sini dönüyor. Fakat bize şu anda localhost gerekiyor.
+            var localMasterIP = masterEndPoint.ToString() switch
+            {
+                "172.18.0.2:6379" => "localhost:1333",
+                "172.18.0.3:6379" => "localhost:1334",
+                "172.18.0.4:6379" => "localhost:1335",
+                "172.18.0.5:6379" => "localhost:1336",
+            };
+
+            ConnectionMultiplexer masterConnection = await ConnectionMultiplexer.ConnectAsync(localMasterIP);
+
+            IDatabase database = masterConnection.GetDatabase();
+            return database;
+        }
+    }
+}
